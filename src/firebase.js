48d031c94d16dbf96005290f1166770678053a9e
Kartik Mishra <kartikm2219@gmail.com>
2022-04-16 20:24:48 +0530

    added new working imports

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,7 @@
-import firebase from "firebase";
+// import firebase from "firebase";
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDZg4pA9PYjJ5xjI2TyTzAVC-Ak5BqETu0",
