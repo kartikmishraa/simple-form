e4eaddaec0a2a0adaa44014022341f58b7af3403
Kartik Mishra <kartikm2219@gmail.com>
2022-04-16 08:52:30 +0530

    base changes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<React.StrictMode>helo</React.StrictMode>);
